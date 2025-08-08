e160392e97a20adb3b4c708269442e3a39c454ae
mrjdaniel25 <mrjdaniel25@gmail.com>
2025-08-08 18:54:41 -0400

    Update route.ts

--- src/app/api/payments/intent/route.ts
+++ src/app/api/payments/intent/route.ts
@@ -1,7 +1,7 @@
 
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
-import { prisma } from '@/lib/prisma'
+import { prisma } from '../../../lib/prisma'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', { apiVersion: '2024-06-20' })
 
 export async function POST(req: NextRequest) {
